1140b77383767aa6ec99abd31c4545328c8183f7
James McMurray <jamesmcm03@gmail.com>
2023-12-08 00:41:36 +0100

    day4 part2 2023

--- src/day4.rs
+++ src/day4.rs
@@ -1,7 +1,11 @@
-use std::{collections::HashSet, hash::RandomState};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::RandomState,
+};
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
+#[derive(Debug, Clone)]
 pub struct Card {
     pub id: u32,
     pub winning_nums: Vec<u32>,
@@ -63,7 +67,30 @@ pub fn solve_part1(input: &[Card]) -> u32 {
 
 #[aoc(day4, part2)]
 pub fn solve_part2(input: &[Card]) -> u32 {
-    todo!()
+    let mut hand: HashMap<u32, u32> = HashMap::new();
+    input.iter().for_each(|c| {
+        hand.insert(c.id, 1);
+    });
+    let max_id = input.iter().map(|c| c.id).max().unwrap();
+
+    for i in 1..=max_id {
+        let c = &input[(i - 1) as usize]; // Assume ordered
+
+        let winning_nums_set: HashSet<u32, RandomState> =
+            HashSet::from_iter(c.winning_nums.iter().cloned());
+        let your_nums_set: HashSet<u32> = HashSet::from_iter(c.your_nums.iter().cloned());
+        let win_count = HashSet::from_iter(your_nums_set)
+            .intersection(&winning_nums_set)
+            .count();
+        for j in 1..=win_count {
+            let this_card_count = *hand.get(&c.id).unwrap();
+            hand.entry(c.id + j as u32)
+                .and_modify(|e| *e += this_card_count)
+                .or_insert(this_card_count);
+        }
+    }
+
+    hand.values().sum()
 }
 
 #[cfg(test)]
@@ -80,5 +107,17 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
         assert_eq!(solve_part1(&input_generator(inp)), 13);
+    }
+
+    #[test]
+    fn test_2() {
+        let inp = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        assert_eq!(solve_part2(&input_generator(inp)), 30);
     }
 }
